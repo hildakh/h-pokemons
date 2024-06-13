3a33420066d58d67b8bb598da430f86252b8bec6
Hilda Khajeh Amiri <hkhajehamiri@usertesting.com>
2024-06-13 12:49:50 -0700

    Add mock global data object

---
+++ server/global-data.js
@@ -0,0 +1,39 @@
+export const globalData = {
+  users: [
+    {
+      id: 1,
+      name: 'Ash Ketchum',
+    },
+    {
+      id: 2,
+      name: 'Misty Blaire',
+    },
+  ],
+  userPokemons: [
+    {
+      id: 1,
+      userId: 1,
+      pokemonId: 1,
+      note: "Just walked around and found it around the yard"
+    }
+  ],
+  pokemons: [
+    {
+      id: 1,
+      name: 'Pikachu',
+      type: 'Electric',
+    },
+    {
+      id: 2,
+      name: 'Bulbasaur',
+      type: 'Grass/Poison',
+    },
+    {
+      id: 3,
+      name: 'Starmie',
+      type: 'Water/Psychic',
+    },
+  ],
+};
+
+
